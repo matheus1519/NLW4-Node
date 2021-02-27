c803ffa391857ab6f2e8d2051c688464e5ea8840
matheus1519 <1519matheus@gmail.com>
2021-02-27 16:18:04 -0300

    NPS, SendMail, Validation: Handle Erros

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,33 +1,52 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
+import { AppError } from "../errors/AppError";
 
 class UserController {
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
 
-  async create(req:Request, res:Response){
-    const {name, email} = req.body;
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    // if (!(await schema.isValid(request.body))) {
+    //   return response
+    //     .status(400)
+    //     .json({ error: "The object must have a certain shape" });
+    // }
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({
+        error: "The object must have a certain shape",
+        errorDetails: err,
+      });
+    }
 
     const usersRepository = getCustomRepository(UsersRepository);
 
     const userAlreadyExists = await usersRepository.findOne({
-      email
-    })
+      email,
+    });
 
-    if(userAlreadyExists){
-      return res.status(400).json({
-        error: "User already exists!"
-      })
+    if (userAlreadyExists) {
+      throw new AppError("User already exists!");
     }
 
     const user = usersRepository.create({
-      name, email
-    })
+      name,
+      email,
+    });
 
     await usersRepository.save(user);
 
-    return res.status(201).json(user);
+    return response.status(201).json(user);
   }
-
 }
 
 export { UserController };
